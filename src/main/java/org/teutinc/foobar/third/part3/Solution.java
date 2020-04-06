f5739b6c4ce3980a590daad963a4e8c1394330c0
Augustin Peyrard <augustin.peyrard@gmail.com>
2020-04-06 13:34:09 -0700

    dummy brute force algo to solve 3.3
    we will obviously have to find a better solution 😈

--- src/main/java/org/teutinc/foobar/third/part3/Solution.java
+++ src/main/java/org/teutinc/foobar/third/part3/Solution.java
@@ -57,6 +57,22 @@ be removed from your home folder.
  */
 public class Solution {
     public static int solution(int[] l) {
-        return 0;
+        if (l.length < 3) {
+            return 0;
+        }
+
+        int numberOfSolutions = 0;
+        for (int i = 0; i < l.length; i++) {
+            for (int j = i + 1; j < l.length; j++) {
+                if (l[j] % l[i] == 0) {
+                    for (int k = j + 1; k < l.length; k++) {
+                        if (l[k] % l[j] == 0) {
+                            numberOfSolutions++;
+                        }
+                    }
+                }
+            }
+        }
+        return numberOfSolutions;
     }
 }
